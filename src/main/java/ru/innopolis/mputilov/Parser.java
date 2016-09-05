a6277b10f190264fc5e1fbaacf9dcd88c2e5061f
Mikhail Putilov <post.snowy@gmail.com>
2016-09-05 22:54:22 +0300

    Add parenthesis support

--- src/main/java/ru/innopolis/mputilov/Parser.java
+++ src/main/java/ru/innopolis/mputilov/Parser.java
@@ -4,6 +4,8 @@ import ru.innopolis.mputilov.expression.*;
 import ru.innopolis.mputilov.expression.Integer;
 import ru.innopolis.mputilov.token.Tokenizer;
 
+import java.util.StringJoiner;
+
 /**
  * Created by mputilov on 04.09.16.
  */
@@ -16,7 +18,7 @@ public class Parser {
     }
 
     public static void main(String[] args) {
-        String rawExpression = "1 + 2     + 4 -5 > 3 and (3 > 5)";
+        String rawExpression = "1 + 2     + 4 -5 > 3 and (3 > (7 + 8))";
         Expression expression = new Parser(rawExpression).parse();
         new PrettyPrinter().print(expression);
     }
@@ -119,16 +121,30 @@ public class Parser {
             String token = iterator.next();
             switch (token) {
                 case "(":
-                    iterator.next();
-                    iterator.next();
-                    iterator.next();
-                    iterator.next();
-                    return new Parenthesized(new Parser("1 < 2").parse());
+                    return new Parenthesized(new Parser(readAllInParenthesis()).parse());
                 default:
                     return new Integer(token);
             }
         }
         throw new RuntimeException("sad");
+    }
+
+    private String readAllInParenthesis() {
+        int openParenthesisCount = 1;
+        StringJoiner insideParenthesis = new StringJoiner(" ");
+        while (iterator.hasNext()) {
+            String token = iterator.next();
+            if (token.equals("(")) {
+                openParenthesisCount++;
+            } else if (token.equals(")")) {
+                openParenthesisCount--;
+            }
+            if (openParenthesisCount == 0) {
+                return insideParenthesis.toString();
+            }
+            insideParenthesis.add(token);
+        }
+        throw new IllegalStateException("");
     }
 
     private Factor.OpCode parseFactorOperator() {
